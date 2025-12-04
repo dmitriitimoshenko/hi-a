9b21009ff08054f3482e37dd590544eeee4e902d
dmitrii <dmitrii.timoshenko16@gmail.com>
2025-12-04 18:48:57 +0100

    SlowThreshold updated (200 default, 500 new (ms))

--- google-sheets-accessor/internal/app/database/client.go
+++ google-sheets-accessor/internal/app/database/client.go
@@ -1,14 +1,30 @@
 package database
 
 import (
+	"log"
+	"os"
+	"time"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func NewConnection(config string) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(config), &gorm.Config{})
+	logCfg := logger.Config{
+		SlowThreshold: time.Millisecond * 500,
+	}
+	gormLogger := logger.New(log.New(os.Stdout, "", log.LstdFlags), logCfg)
+
+	db, err := gorm.Open(
+		postgres.Open(config),
+		&gorm.Config{
+			Logger: gormLogger,
+		},
+	)
 	if err != nil {
 		return db, err
 	}
+
 	return db, nil
 }

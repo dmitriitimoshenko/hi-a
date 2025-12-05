ed3a06482f9b4dacb4eb1f63a3abf4728517fe2f
dmitrii <dmitrii.timoshenko16@gmail.com>
2025-12-05 16:47:14 +0100

    testing logs added

--- google-sheets-accessor/internal/pkg/services/application.go
+++ google-sheets-accessor/internal/pkg/services/application.go
@@ -734,6 +734,13 @@ func (s *ApplicationService) getApplicationDiff(
 		}
 	}
 
+	s.logger.Info(
+		"salary applied check",
+		slog.Any("db", dbAppliedSalaryDTO.ToString()),
+		slog.Any("sh", sheetAppliedSalaryDTO.ToString()),
+		slog.Bool("*dbAppliedSalaryDTO != *sheetAppliedSalaryDTO", *dbAppliedSalaryDTO != *sheetAppliedSalaryDTO),
+	)
+
 	if dbAppliedSalaryDTO != nil && sheetAppliedSalaryDTO != nil &&
 		*dbAppliedSalaryDTO != *sheetAppliedSalaryDTO {
 		diffs = append(diffs, dto.ApplicationDiff{
